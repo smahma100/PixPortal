3198f19b699b7e921db97d12a9ebf65230fe8658
safar <smahma100@gmail.com>
2024-10-15 12:10:32 -0700

    chore: update log files and configuration in PhotoApp services
    - Modified albums-ws.log in Album service
    - Updated Photo-App-Api-Users.log in PhotoAppApiUsers service
    - Updated pom.xml in PhotoAppDiscoveryService
    - Modified application.properties in PhotoAppDiscoveryService
    - Added WebSecurityConfig.java in PhotoAppDiscoveryService

---
+++ PhotoAppDiscoveryService/src/main/java/com/saberdot/photoapp/dicovery/WebSecurityConfig.java
@@ -0,0 +1,31 @@
+package com.saberdot.photoapp.dicovery;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())  // Disable CSRF for simplicity
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()  // Require authentication for any request
+                )
+                .httpBasic();  // Enable basic authentication
+
+        return http.build();
+    }
+
+}
